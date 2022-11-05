ba32f12c3850b40ff495daf3fb6cee5342d869a1
Reiya Dave <reiya.dave@gmail.com>
2022-11-05 11:46:50 -0400

    refactor fix

--- src/main/java/com/example/project3/GymManagerController.java
+++ src/main/java/com/example/project3/GymManagerController.java
@@ -3,7 +3,7 @@ package com.example.project3;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class GymManagerController {
     @FXML
     private Label welcomeText;
 
