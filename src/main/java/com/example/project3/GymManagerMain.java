1f721944dc7f7037a002aac2388e5f029b83b420
Ifrah Sajjad <67663463+isajjad1246@users.noreply.github.com>
2022-11-06 22:24:17 -0500

    setting stage title

--- src/main/java/com/example/project3/GymManagerMain.java
+++ src/main/java/com/example/project3/GymManagerMain.java
@@ -12,12 +12,13 @@ public class GymManagerMain extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GymManagerMain.class.getResource("GymManagerView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Gym Manager");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
+
         launch();
     }
 }
